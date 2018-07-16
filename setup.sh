102b68b77bff1170a7e7246fdf630dfa80d3a2e9
Alex Su <alexsu@alexsu-vm.dev.corp.dropbox.com>
2018-07-16 20:43:18 +0000

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set up script for configuration files
+
+# Set up symlinks
+
+ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/vim/.vim ~/.vim
+ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
